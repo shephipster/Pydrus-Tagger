59fe0a952d1e9ea20f1936bcd9fdd784f9df3b3a
shephipster <shephipster@gmail.com>
2021-10-29 18:47:12 -0400

    Added PHP support
    PHP encoding is now cleaned up. For example, ninomae_ina'nis now prints as such instead of ninomae_ina&039;nis

--- Tagger.py
+++ Tagger.py
@@ -9,7 +9,8 @@ class Tagger:
     #Tags in a list of tags and cleans items up
     #For example, %27 changed back to ' and remove # from twitter
     def getCleanTag(tag):
-        cleanTag = str(tag)        
+        cleanTag = str(tag) 
+        #HTML  
         cleanTag = cleanTag.replace("#", '')
         cleanTag = cleanTag.replace("%21", '!') 
         cleanTag = cleanTag.replace('%23', '#') 
@@ -29,7 +30,13 @@ class Tagger:
         cleanTag = cleanTag.replace('%3F', '?') 
         cleanTag = cleanTag.replace('%40', '@') 
         cleanTag = cleanTag.replace('%5B', '[') 
-        cleanTag = cleanTag.replace('%5D', ']') 
+        cleanTag = cleanTag.replace('%5D', ']')
+        #PHP
+        cleanTag = cleanTag.replace('&039;', '\'')
+        cleanTag = cleanTag.replace('&amp;', '&')
+        cleanTag = cleanTag.replace('&quot;', '\"')
+        cleanTag = cleanTag.replace('&lt;', '<')
+        cleanTag = cleanTag.replace('&gt;', '>')
         return cleanTag
 
     def getGelbooruTags(references):
